17a0d94a7fcea88479d485d532cae1c9ffa3baa4
TikhonP <tikhonp@users.noreply.github.com>
2022-10-21 16:15:10 +0300

    Initial Commit

---
+++ Medsenger/MedsengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsengerApp.swift
+//  Medsenger
+//
+//  Created by Tikhon Petrishchev on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MedsengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
