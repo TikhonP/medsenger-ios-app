0c62e5fda29a12c6e40ceffae5fee82d53ce598d
TikhonP <tikhonp@users.noreply.github.com>
2022-11-07 18:10:41 +0300

    Added messages request

---
+++ MedsengerKit/Messages/MessagesRequest.swift
@@ -0,0 +1,23 @@
+//
+//  MessagesRequest.swift
+//  Medsenger
+//
+//  Created by Tikhon Petrishchev on 07.11.2022.
+//  Copyright © 2022 TelePat ltd. All rights reserved.
+//
+
+import Foundation
+
+struct MessagesResource: APIResource {
+    let contractId: Int
+    
+    typealias ModelType = Array<Message.JsonDeserializer>
+    
+    var methodPath: String {
+        "/doctors/\(contractId)/messages"
+    }
+    var options = APIResourceOptions(
+        dateDecodingStrategy: .secondsSince1970,
+        parseResponse: true
+    )
+}
